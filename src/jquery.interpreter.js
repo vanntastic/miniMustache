53d8f7e04ac006f73ecf3bb07b59c503c030a461
vann <vann@innerfusion.net>
2010-09-25 16:28:28 -0500

    add in support for passing in a collection of objects and updated example page to improve that

---
+++ src/jquery.interpreter.js
@@ -0,0 +1,72 @@
+;(function($) {
+  
+// Interpreter a simple jquery plugin/engine that allows you to embed object values in your page using a bracket style syntax.
+
+$.fn.interpreter = function(obj, options) {
+  var opts = $.extend({}, $.fn.interpreter.defaults, options);
+
+  if (typeof(this) == "string") {
+    return this.interpret(obj);
+  }else{
+    return this.each(function() {
+
+      var $this = $(this);
+      
+      if (obj.length == undefined) {
+        // not an array of objects
+        $this.html($this.html().interpret(obj));
+      }else{
+        // it's an array of objects and we will need to create the collection as
+        // necessary
+        // ensure that the dom item is unique, so we can use it as a template
+        var template = $this.children(opts.collectionClass);
+        var templateId = randomId();
+        template.attr('id',templateId);
+        templateHtml = template.html();
+        for (var i=0; i < obj.length; i++) {
+          var _id = "#" + templateId;
+          if (i==0) {
+            $(_id).html(templateHtml.interpret(obj[i]));
+            var parsedContent = $(_id).html().interpret(obj[i]);
+          }else{
+            var newItem = $(_id).clone().attr('id',randomId());
+            newItem.html(templateHtml.interpret(obj[i])).appendTo($this);
+          };
+        };
+      };
+      
+      // Support for the Metadata Plugin.
+      var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
+
+    });
+  };
+  
+  function randomId () {
+    var idName = opts.collectionClass.split(".")[1];
+    return idName + "-" + Math.floor(Math.random()*9999999);
+  }
+
+  // private function for debugging
+  function debug($obj) {
+    if (window.console && window.console.log) {
+      window.console.log($obj);
+    }
+  }
+};
+
+// default options
+$.fn.interpreter.defaults = {
+  collectionClass: ".collection"
+};
+
+})(jQuery);
+
+// Borrowed from : http://javascript.crockford.com/remedial.html
+String.prototype.interpret = function (o) {
+    return this.replace(/{([^{}]*)}/g,
+        function (a, b) {
+            var r = o[b];
+            return typeof r === 'string' || typeof r === 'number' ? r : a;
+        }
+    );
+};
